b807bd88ed03fc96e4f044876894afae8d65bb8c
Wheeler <wheelera@CSGP.EDU@CSC-20.CSGP.EDU>
2025-08-27 07:42:58 -0600

    Main Code

---
+++ main.py
@@ -0,0 +1,39 @@
+name = input("What your name?")
+if name != "Lebron" or name != "Lebron James":
+    print("Oh, ok loser.")
+else:
+    print("OH MY GOD MY GLORIOUS KING I LOVE YOU!")
+
+age = int(input("How old are you?"))
+if age > 16:
+    print("Damn, old ahh jit. What was Rosa Parks really like?")
+if age < 16:
+    print("Damn, bro young ahh. You missed everything ngl. Fetus lookin ahh")
+if age == 16:
+    print("Real")
+
+food = input("What's your favorite food?")
+if food != "Chicken and Rice":
+    print("Sounds like someone needs to cut.")
+else:
+    print("Locked in.")
+
+color = input("What's your favorite color?")
+if color == "Blue" or "Red" or "Green":
+    print("Pretty basic ngl.")
+if color == "Black":
+    print("Okay edgelord.")
+if color == "Purple" or "Pink":
+    print("Bruh. Cornball lookin ahh.")
+if color == "White":
+    print("Based.")
+else:
+    print("Goofy art student ahh. Get a real job picasso.")
+
+sport = input("What's your favorite sport?")
+if sport == "Gooning":
+    print("Respectable")
+if sport == "Badminton":
+    print("Got it ON LOCK")
+else:
+    print("Goofy ahh playing that sploinky doinky")
