828db82afcf41a503226ef38dca83086cca46671
AcaciaBean <andrew.d.wheeler09@gmail.com>
2025-09-02 13:19:44 -0600

    Fix to all if statements

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-name = input("What your name?")
-if name == "Lebron" or "Lebron James":
+name = str(input("What your name? "))
+if name == "Lebron" or name == "Lebron James":
     print("OH MY GOD MY GLORIOUS KING I LOVE YOU!")
 else:
     print("Oh, ok loser.")
 
-age = int(input("How old are you?"))
+age = int(input("How old are you? "))
 if age > 16:
     print("Damn, old ahh jit. What was Rosa Parks really like?")
-if age < 16:
+elif age < 16:
     print("Damn, bro young ahh. You missed everything ngl. Fetus lookin ahh")
-if age == 16:
+elif age == 16:
     print("Real")
 
-food = input("What's your favorite food?")
+food = input("What's your favorite food? ")
 if food != "Chicken and Rice":
     print("Sounds like someone needs to cut.")
 else:
     print("Locked in.")
 
-color = input("What's your favorite color?")
+color = str(input("What's your favorite color? "))
 if color == "Blue" or color == "Red" or color == "Green":
     print("Pretty basic ngl.")
-if color == "Black":
+elif color == "Black":
     print("Okay edgelord.")
-if color == "Purple" or "Pink":
+elif color == "Purple" or "Pink":
     print("Bruh. Cornball lookin ahh.")
-if color == "White":
+elif color == "White":
     print("Based.")
 else:
     print("Goofy art student ahh. Get a real job picasso.")
 
-sport = input("What's your favorite sport?")
+sport = str(input("What's your favorite sport? "))
 if sport == "Gooning":
     print("Respectable")
 elif sport == "Badminton":
